e0515586395cd0f57ff0bbbf6d88249ea27413f5
Mharti-Mudcrab <48588809+Mharti-Mudcrab@users.noreply.github.com>
2024-09-26 12:13:38 +0200

    Cleanned up .bats

--- AutoDeploymentScripts/DeployAll_old.bat
+++ AutoDeploymentScripts/DeployAll_old.bat
@@ -1,17 +1,17 @@
 @echo off
 
-echo ========== Deploy a Kafka cluster - Ref: Lecture 2 Exercise 1 ==========
+echo ========== Deploy a Kafka cluster - Ref: Lecture 3 Exercise 1 ==========
 helm install --values kafka-values.yaml kafka oci://registry-1.docker.io/bitnamicharts/kafka --version 30.0.4
-kubectl run kafka-client --restart='Never' --image docker.io/bitnami/kafka:3.8.0-debian-12-r3  --command -- sleep infinity
+kubectl run kafka-client --restart=Never --image docker.io/bitnami/kafka:3.8.0-debian-12-r3  --command -- sleep infinity
 
-echo ========== Deploy a Kafka Connect, Kafka Schema Registry, and Kafka KSQL - Ref: Lecture 2 Exercise 2 ==========
-cd /d %~dp0lectures\03
+echo ========== Deploy a Kafka Connect, Kafka Schema Registry, and Kafka KSQL - Ref: Lecture 3 Exercise 2 ==========
+cd /d %~dp0\..\lectures\03
 kubectl apply -f kafka-schema-registry.yaml 
 kubectl apply -f kafka-connect.yaml
 kubectl apply -f kafka-ksqldb.yaml
 
-echo ========== Deploy Redpanda - Ref: Lecture 2 Exercise 3 ==========
-cd /d %~dp0lectures\03
+echo ========== Deploy Redpanda - Ref: Lecture 3 Exercise 3 ==========
+cd /d %~dp0\..\lectures\03
 kubectl apply -f redpanda.yaml
 setlocal
 :: Define the path for the temporary file
@@ -20,7 +20,7 @@ set "tempFile=%TEMP%\autoexec_temp.bat"
 (
 	echo @echo off
     echo echo Access Redpanda with: http://127.0.0.1:8080
-	echo echo Waiting for pod Ready (timeout=10s) . . .
+	echo echo Waiting for pod Ready ^(timeout=10s^) . . .
     echo kubectl wait --for=condition=Ready pod -l app=redpanda --timeout=10s
 	echo echo Start port-forward
 	echo kubectl port-forward svc/redpanda 8080
@@ -30,9 +30,8 @@ start "" "%tempFile%"
 endlocal
 
 echo ========== Deploy HDFS - Ref: services\hdfs ==========
-cd /d %~dp0services\hdfs
+cd /d %~dp0\..\services\hdfs
 kubectl apply -f configmap.yaml
-kubectl get configmap hadoop-config
 kubectl apply -f namenode.yaml
 echo Waiting for namenode condition Ready (timeout=10s) . . .
 kubectl wait --for=condition=Ready pod -l app=namenode --timeout=10s
@@ -41,7 +40,7 @@ kubectl apply -f datanodes.yaml
 kubectl apply -f hdfs-cli.yaml
 
 echo ========== Deploy interactive pod - Ref: services\interactive - Alt. apache/hadoop:3 Ref: lecture 2 Exercise 2 ==========
-cd /d %~dp0services\interactive
+cd /d %~dp0\..\services\interactive
 start cmd /k "echo Interactive pod! & kubectl run interactive -i --tty --image registry.gitlab.sdu.dk/jah/bigdatarepo/interactive:latest -- /bin/bash"
 ::start cmd /k "echo Interactive hadoop:3 pod! & kubectl run hadoop3 -i --tty --image apache/hadoop:3 -- /bin/bash"
 
